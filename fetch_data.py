40e2651941e349fbc82ed5e2074c8085aafbad74
georgiygusev <georgiy.gus@yandex.ru>
2025-07-20 00:06:02 +0300

    init

---
+++ fetch_data.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+
+import aiohttp
+
+from utils import chunked
+
+GET_ALL_ADS_URL = "https://advert-api.wildberries.ru/adv/v1/promotion/count"
+GET_ADS_STATS_URL = "https://advert-api.wildberries.ru/adv/v2/fullstats"
+GET_ADS_INFO_URL = "https://advert-api.wildberries.ru/adv/v1/promotion/adverts"
+
+
+# <ts> это дата когда выполняется функция, найди от нее вчерашнюю дату и используй
+async def fetch_data(api_token: str, campaign_ids: list, load_date: str) -> list:
+    headers = {"Authorization": api_token}
+    dt_ts = datetime.fromisoformat(load_date)
+    yesterday = (dt_ts - timedelta(days=1)).strftime("%Y-%m-%d")
+    result = []
+    body = [{"id": cid, "dates": [yesterday]} for cid in campaign_ids]
+    async with aiohttp.ClientSession(headers=headers) as session:
+        for batch in chunked(body, 100):
+            async with session.post(GET_ADS_STATS_URL, body=batch) as response:
+                data = await response.json()
+                response.raise_for_status()
+                result.extend(data)
+    return result
