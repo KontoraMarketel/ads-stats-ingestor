c154ff78ca3782067a9a77173090c47a092e2570
georgiygusev <georgiy.gus@yandex.ru>
2025-09-23 00:14:03 +0300

    remove example.py

--- example.py
+++
@@ -1,9 +0,0 @@
-from utils import chunked
-
-# Пример:
-data = list(range(1, 353))
-dates_chunked = list(chunked(data, 10))
-
-
-body = [{"ids": cid, "dates": ["okeoke"]} for cid in dates_chunked]
-print(body)
